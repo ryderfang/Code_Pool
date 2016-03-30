c9497508cb4b80344f0374580af1f23937245305
FongRay <fray9166@gmail.com>
2016-03-30 18:21:27 +0800

    kmp

---
+++ common/kmp.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+[arr, &next]() {
+    next[0] = next[1] = 1;
+    for (size_t i = 2; i < arr
+}
+
+void simple_kmp(const string& ori, const string& pat) {
+     
+}
+
+int main() {
+    string text = "this is the origin text";
+    string pattern = "the";
+    return 0;
+}
+
