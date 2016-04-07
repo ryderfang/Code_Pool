11b4bab1bd19bfb252d7c0318afc6f54da4fb23e
FongRay <fray9166@gmail.com>
2016-04-07 15:39:23 +0800

    lc002

---
+++ leetcode/lc002_add_two_numbers.cc
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* p1 = l1;
+        ListNode* p2 = l2;
+        int carry = 0;
+        ListNode* prev = NULL;
+        ListNode* head = NULL;
+        while (p1 != NULL || p2 != NULL) {
+            ListNode* p_sum = new ListNode(0);
+            if (p1 == NULL) p_sum->val = p2->val;
+            else if (p2 == NULL) p_sum->val = p1->val;
+            else p_sum->val = p1->val + p2->val;
+            p_sum->val += carry;
+            carry = p_sum->val / 10;
+            p_sum->val %= 10;
+            if (p1) p1 = p1->next;
+            if (p2) p2 = p2->next;
+            if (prev) prev->next = p_sum;
+            else head = p_sum;
+            prev = p_sum;
+        }
+        return head;
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    ListNode* p1_1 = new ListNode(1);
+    ListNode* p1_2 = new ListNode(8);
+    p1_1->next = p1_2;
+
+    ListNode* p2_1 = new ListNode(0);
+
+    cout << sol.addTwoNumbers(p1_1, p2_1) << endl;
+    return 0;
+}
