fbe3c9a1d7f448a7bc77c8df95ab723fb70abd1f
fangrui <fangrui@bilibili.com>
2017-06-07 00:54:45 +0800

    py++

--- leetcode/python/lc167_two_sum_II_input_array_is_sorted.py
+++ leetcode/python/lc167_two_sum_II_input_array_is_sorted.py
@@ -5,7 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(numbers)):
+        i = 0
+        while i < len(numbers):
             sub = numbers[i + 1:]
             if sub.count(target - numbers[i]) > 0:
                 return [i + 1, sub.index(target - numbers[i]) + i + 2]
+            else:
+                i = i + numbers.count(numbers[i])
