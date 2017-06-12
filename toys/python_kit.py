924cfee20e800df97d01642578675a14433fb068
fangrui <fangrui@bilibili.com>
2017-06-12 18:05:08 +0800

    py+2

--- toys/python_kit.py
+++ toys/python_kit.py
@@ -41,5 +41,87 @@ def comp_big_int(a, b):
         i += 1
     return 0
 
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+# list to binary tree
+def list_to_binary_tree(l):
+    if len(l) == 0 or l[0] == None:
+        return None
+    queue = []
+    root = TreeNode(l[0])
+    queue.append(root)
+    i = 1
+    while len(queue) > 0 and i < len(l):
+        print(queue)
+        left = None
+        if i < len(l):
+            left = TreeNode(l[i])
+        i += 1
+        right = None
+        if i < len(l):
+            right = TreeNode(l[i])
+        i += 1
+        queue[0].left = left
+        queue[0].right = right
+        queue = queue[1:]
+        queue.append(left)
+        queue.append(right)
+    return root
+
+# 前序 - 根左右
+def pre_order_binary_tree(root):
+    ans = []
+    stack = [root]
+    while len(stack) > 0:
+        top = stack.pop()
+        if top != None:
+            ans.append(top.val)
+            stack.append(top.right)
+            stack.append(top.left)
+    return ans
+
+# 中序 - 左根右
+def mid_order_binary_tree(root):
+    ans = []
+    stack = []
+    p = root
+    while p != None or len(stack) > 0:
+        while p != None:
+            stack.append(p)
+            p = p.left
+        if len(stack) > 0:
+            p = stack.pop()
+            ans.append(p.val)
+            p = p.right
+    return ans
+
+# 后序 - 左右根
+def post_order_binary_tree(root):
+    ans = []
+    stack = []
+    stack.append((root, False))
+    while len(stack) > 0:
+        top = stack.pop()
+        p = top[0]
+        visited = top[1]
+        if p == None:
+            continue
+        if visited:
+            ans.append(p.val)
+        else:
+            stack.append((p, True))
+            stack.append((p.right, False))
+            stack.append((p.left, False))
+    return ans
+
+def tree_height(root):
+    if root == None:
+        return 0
+    return 1 + max(tree_height(root.left), tree_height(root.right))
+
+
 if __name__ == '__main__':
     pass
