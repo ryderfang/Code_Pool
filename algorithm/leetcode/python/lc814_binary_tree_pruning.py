85b0552757714f0d0e31514efbe08e85519ab8a4
以牧 <fr148791@alibaba-inc.com>
2018-05-29 20:50:10 +0800

    :beer: lc814

---
+++ algorithm/leetcode/python/lc814_binary_tree_pruning.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def pruneTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return None
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if root.left == None and root.right == None and root.val == 0:
+            return None
+        return root
+    
+import sys
+sys.path.append(sys.path[0] + '/../../../')
+print(sys.path)
+from toys import python_kit
+
+if __name__ == '__main__':
+    sol = Solution()
+    t = sol.pruneTree(python_kit.list_to_binary_tree([1,1,0,1,1,0,1,0]))
+    print(python_kit.level_order_binary_tree(t))
